4c4d9fbeed68689f30d4930e252724c74c60ab8f
Legend0b0 <gustavoolsilva11@gmail.com>
2024-02-28 23:03:26 -0300

    Exclusion of hidden directories

--- AttGits.sh
+++ AttGits.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 n=0
-for i in $(find /home/gustavo/ -iname ".git")
+for i in $(find /home/gustavo/ -regex "/home/gustavo/[A-Za-z0-9].*\.git")
 do
   n=$((n+1))
 done
-array=($(find /home/gustavo/ -iname ".git" | sed s/.git//g))
+array=($(find /home/gustavo/ -regex "/home/gustavo/[A-Za-z0-9].*\.git" | sed s/.git//g))
 for ((i=0; i<$n; i++))
 do
   echo -e "\e[0;32m${array[$i]}\e[0m"
   cd ${array[$i]}
   git status -s
-  sleep 0.2
+  sleep 0.15
 done
 cd
