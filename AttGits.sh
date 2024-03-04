9a4cae6b5600be6ed683444b9eac64def6810297
Legend0b0 <gustavoolsilva11@gmail.com>
2024-03-04 16:33:53 -0300

    Generalization of path

--- AttGits.sh
+++ AttGits.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 n=0
-for i in $(find /home/gustavo/ -regex "/home/gustavo/[A-Za-z0-9].*\.git")
+for i in $(find /home/gustavo/ -regex "$HOME/[A-Za-z0-9].*\.git")
 do
   n=$((n+1))
 done
-array=($(find /home/gustavo/ -regex "/home/gustavo/[A-Za-z0-9].*\.git" | sed s/.git//g))
+array=($(find /home/gustavo/ -regex "$HOME/[A-Za-z0-9].*\.git" | sed s/.git//g))
 for ((i=0; i<$n; i++))
 do
   echo -e "\e[0;32m${array[$i]}\e[0m"
